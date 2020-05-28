65b960b49943d7a30b97e2ecf946c5bbcf11fe46
Sam Carlton <ThatGuySam@users.noreply.github.com>
2020-05-28 13:16:50 -0500

    Update menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,10 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  { label: 'churchprod', page: '/' },
+//   { label: 'Blog', page: '/' },
+//   { label: 'Contact', page: '/contact' },
+//   { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
