951a524e6d6d9d8ac8b3e4ad2d4285dfbc68c998
Sam Carlton <ThatGuySam@users.noreply.github.com>
2020-05-28 13:17:38 -0500

    Update title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,7 +19,7 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} churchprod</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
